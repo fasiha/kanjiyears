318450a1174066858682d11d462cff5e152dee47
Ahmed Fasih <wuzzyview@gmail.com>
2015-03-17 12:09:54 -0400

    Colors and some stats

--- years.js
+++ years.js
@@ -14,8 +14,7 @@ function getOnline(url) {
   });
 }
 
-// Cache this since presumably it might be used regularly: regular expression to capture
-// all Chinese characters
+// Regular expression to capture all Chinese characters, via XRegExp
 var hanRegexp = XRegExp('\\p{Han}', 'g');
 
 // Setup
@@ -36,6 +35,26 @@ getOnline('kanken.json').then(function(json) {
   kanjiToKanken = _.object(_.flatten(_.map(kankenToKanji, function(kanjis, kanken) {
     return kanjis.map(function(kanji) { return [ kanji, kanken ]; });
   })));
+});
+
+// From Kanken.or.jp, colors: http://www.kanken.or.jp/kanken/outline/degree.html
+var KANKENCOLORS = {
+  1 : "#004129",
+  1.5 : "#690212",
+  2 : "#00437C",
+  2.5 : "#E8380D",
+  3 : "#008A6C",
+  4 : "#B81649",
+  5 : "#F7AB00",
+  6 : "#5B1F67",
+  7 : "#0096BB",
+  8 : "#C4D700",
+  9 : "#6D70B3",
+  10 : "#E00083"
+};
+// Kanken levels 10--5 correspond to grades 1--6.
+_.range(1, 6 + 1).map(function(grade) {
+  return KANKENCOLORS['grade-' + grade] = KANKENCOLORS[10 - grade + 1];
 });
 
 // Business logic: returns an object whose keys are grades (or "undefined"), and values an
@@ -80,7 +99,7 @@ function run() {
   //     input, `completeHash` has the *full* group. If available, we will show what's in
   //     each group but missing in the input.
   function displayResults(domSelectionName, hash, hashKeysArray, headingFunction,
-                          completeHash) {
+                          completeHash, colorLookupPrefixString) {
     // Clear old display
     d3.select(domSelectionName).html('');
 
@@ -92,7 +111,7 @@ function run() {
     // well as `hash`), we'll only show this many kanji.
     var listTruncationLimit = Math.floor(
         d3.select(domSelectionName).node().getBoundingClientRect().width / ichiWidth() -
-        2);
+        3);
 
     // For each *group* (each key in `hash`), create a div to hold it, and then just
     // append to those divs all the elements of interest.
@@ -101,7 +120,10 @@ function run() {
                    .data(hashKeysArray ? hashKeysArray : _.keys(hash))
                    .enter()
                    .append('div')
-                   .classed('data-box', true);
+                   .classed('data-box', true)
+                   .style("border-color", function(d, i) {
+                     return KANKENCOLORS[(colorLookupPrefixString || "") + d] || "white";
+                   });
 
     // Append heading
     divs.append('p')
@@ -131,14 +153,10 @@ function run() {
     // could all be stuffed into a single html() call, but I wanted to break up the
     // contents using `d3.append` more than that.
     var processMissing = _.memoize(function(d) {
-      var missing = (completeHash && completeHash[d])
-                        ? _.difference(completeHash[d], hash[d]).join('')
-                        : '';
-      return {
-        kanji : missing,
-        show : missing.slice(0, listTruncationLimit),
-        hide : missing.slice(listTruncationLimit)
-      };
+      return (completeHash && completeHash[d])
+                 ? _.difference(completeHash[d], hash[d]).join('')
+                 : '';
+
     });
     // Append the first line of missing kanji, then an ellipses, and then in a hidden
     // span, the rest of them. Add the event listener to unhide them.
@@ -146,24 +164,20 @@ function run() {
         .html(
              function(d, i) {
                var missing = processMissing(d);
-               return (missing.show) ? 'Missing kanji:<br>' + missing.show : '';
+               if (missing.length === 0) {
+                 return "";
+               }
+               var moreLines = Math.ceil(missing.length / listTruncationLimit);
+               return '<span class="ellipses">(show missing kanji, ' + moreLines +
+                      ' row' + (moreLines > 1 ? 's' : '') +
+                      ')</span><span class="hidden">（' + missing + '）</span>';
              })
-        .classed('missing-kanji', true)
-        .append('span')
-        .html(
-             function(d, i) {
-               var missing = processMissing(d);
-               var moreLines = Math.ceil(missing.hide.length / listTruncationLimit);
-               return missing.hide
-                          ? '<span class="ellipses">…<br>(' + moreLines + ' more row' +
-                                (moreLines > 1 ? 's' : '') +
-                                ')</span><span class="hidden">' + missing.hide + '</span>'
-                          : '';
-             })
+        .classed('missing-kanji linkable', true)
         .on('click', function(d) {
           var self = d3.select(this);
           self.select('.ellipses').remove();
           self.select('.hidden').classed('hidden', false);
+          self.classed('linkable', false);
         });
 
     // A button that'll show all the hidden content (and then deletes itself). This might
@@ -172,7 +186,7 @@ function run() {
       d3.select(domSelectionName)
           .append('button')
           .classed('pure-button show-all-button', true)
-          .html('Show all hidden?')
+          .html('Show all missing kanji')
           .on('click', function() {
             d3.selectAll('.hidden').classed('hidden', false);
             d3.selectAll('.ellipses').remove();
@@ -191,7 +205,7 @@ function run() {
       heading = "Secondary school";
     }
     return heading;
-  }, gradeToKanji);
+  }, gradeToKanji, 'grade-');
 
   // Display non-joyo and secondary school kanji by kanken
   displayResults(
@@ -199,4 +213,43 @@ function run() {
       _.sortBy(_.keys(kankenToRareKanji), function(x) { return x ? -x : NaN; }),
       function(d) { return d !== 'undefined' ? 'Kanken ' + d : 'Non-kanken'; },
       kankenToKanji);
+
+  var stats = _.sortBy(_.range(10, 2 - 1, -1).concat([2.5]), function(x) { return -x; })
+                  .map(function(kanken) {
+    var summary;
+    if (kanken >= 5) {
+      var grade = 10 - kanken + 1;
+      summary = {
+        description : 'Grade ' + grade,
+        seen : (inputGradeToKanji[grade] || []).length,
+        total : gradeToKanji[grade].length,
+        color : d3.hcl(KANKENCOLORS['grade-' + grade])
+      };
+    } else {
+      summary = {
+        description : 'Kanken ' + kanken,
+        seen : (kankenToRareKanji[kanken] || []).length,
+        total : kankenToKanji[kanken].length,
+        color : d3.hcl(KANKENCOLORS[kanken])
+      };
+    }
+    return summary;
+  });
+  d3.select('#stats-output').html('');
+  var boxes =
+      d3.select('#stats-output')
+          .selectAll('div')
+          .data(stats)
+          .enter()
+          .append('div')
+          .classed('outer-box', true)
+          .style('background', function(d) { return d.color.brighter(1.25).toString(); });
+  boxes.append('div')
+      .classed('inner-box', true)
+      .style('height', function(d, i) { return (100 * d.seen / d.total) + '%'; })
+      .style('background', function(d) { return d.color.toString(); })
+      .style('color', function(d) { return (d.color.l > 70 ? "black" : "white"); })
+      .text(function(d, i) { return d.description + ' ' + d.seen + '/' + d.total; });
+
+  return {inputGrade : inputGradeToKanji, inputKanken : kankenToRareKanji, stat : stats};
 }
